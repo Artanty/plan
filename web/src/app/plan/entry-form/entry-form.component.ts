74e62b8b9eb37c640286f93941696339f4e62afb
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-05-10 00:36:01 +0300

    * web: PLAN-0003 add default values on task create: ready https://red-kqjc.onrender.com/plan/get-ext-task/PLAN-0003

--- web/src/app/plan/entry-form/entry-form.component.ts
+++ web/src/app/plan/entry-form/entry-form.component.ts
@@ -1,13 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Observable, map, share, shareReplay, switchMap, tap } from 'rxjs';
+import { EMPTY, Observable, catchError, finalize, map, share, shareReplay, switchMap, tap } from 'rxjs';
 import { DrawerService } from '../components/drawer/drawer.service';
 import { ICreateTaskApi } from '../../../../../contracts/createTask'
-import { TBaseOption, getServiceNameById, greaterThanZeroValidator, mapSelectOptions, prepareStatus4Api, taskStatusOptions as statuses } from '../constants'
+import { IEditTaskApi } from '../../../../../contracts/editTask'
+import { TBaseOption, greaterThanZeroValidator, mapSelectOptions, prepareStatus4Api, taskStatusOptions as statuses } from '../constants'
 import { IGetUserExternalsApi } from '../../../../../contracts/getUserExternals';
 import { TSelectExternals } from '../entries-list/entries-list.component';
-
+import { ITaskFormValue, TaskStoreService } from '../store/task/task-store.service';
+import { UserExternalStoreService } from '../store/userExternal/user-external-store.service';
+import { UserExternalService } from '../services/user-external.service';
+import { TaskService } from '../services/task.service';
+import { TaskApiService } from '../api/task/task-api.service';
 
 @Component({
   selector: 'app-entry-form',
@@ -18,6 +23,7 @@ import { TSelectExternals } from '../entries-list/entries-list.component';
   ]
 })
 export class EntryFormComponent {
+  Number = Number
   form: FormGroup;
   externals$: Observable<TSelectExternals[]>;
   taskStatusOptions: TBaseOption[] = [
@@ -25,12 +31,18 @@ export class EntryFormComponent {
     ...statuses.map((el: string, i: number) => ({ id: i + 1, name: el }))
   ]
   resetDatePicker: boolean = false
-  due_date: string = ''
+  due_date: string = new Date().toISOString().split('T')[0]
+  due_date_result: string = ''
 
   constructor(
     private fb: FormBuilder,
     @Inject(HttpClient) private http: HttpClient,
-    private drawerService: DrawerService
+    @Inject(DrawerService) private drawerService: DrawerService,
+    @Inject(TaskStoreService) private taskStore: TaskStoreService,
+    @Inject(UserExternalStoreService) private userExternalStore: UserExternalStoreService,
+    @Inject(UserExternalService) private userExternalService: UserExternalService,
+    @Inject(TaskService) private taskService: TaskService,
+    @Inject(TaskApiService) private taskApi: TaskApiService
   ) {
     // date
     this.resetDatePicker = true // do not remove
@@ -40,10 +52,19 @@ export class EntryFormComponent {
       description: [''],
       externalServiceId: [-1, [Validators.required, greaterThanZeroValidator()]],
       externalServiceName: [''],
-      status: [-1, [Validators.required, greaterThanZeroValidator()]]
+      status: [-1, [Validators.required, greaterThanZeroValidator()]],
+      external_task_id: [''],
+      id: ['']
     });
 
-    this.externals$ = this.getExternals()
+    this.externals$ = this.userExternalStore.listenUserExternals().pipe(
+      map(res => {
+        return [
+          { id: -1, name: 'Проект' },
+          ...mapSelectOptions(res)
+        ]
+      })
+    )
     this.form.valueChanges.subscribe(res => {
       this.resetValidation()
     })
@@ -56,12 +77,40 @@ export class EntryFormComponent {
     //   externalServiceId: 1,
     //   externalServiceName: 'doro@github'
     // })
+    this.taskStore.listenTaskToEdit().subscribe((res: ITaskFormValue | null) => {
+      if (res) {
+        this.form.patchValue(res)
+      } else {
+        this.onReset()
+        const data = this.getDefaultFormData()
+        this.form.patchValue(data)
+      }
+    })
   }
-  creds: any = ''
+
+  private getDefaultFormData(): ITaskFormValue {
+    const externalFilterVal = this.userExternalStore.getUserExternalFilter()
+    const externalServiceName = externalFilterVal
+    ? this.userExternalService.getServiceNameById(externalFilterVal)
+    : ''
+    const initialData: ITaskFormValue = {
+      title: '',
+      description: '',
+      status: 1,
+      due_date: new Date().toISOString().split('T')[0],
+      externalServiceId: externalFilterVal || -1,
+      externalServiceName: externalServiceName
+    }
+    return initialData
+  }
 
   onSubmit() {
     if (this.form.valid) {
-      this.prepareAndCreateTask()
+      if (this.form.value.id) {
+        this.prepareAndEditTask()
+      } else {
+        this.prepareAndCreateTask()
+      }
     } else {
       this.form.markAllAsTouched()
     }
@@ -85,40 +134,45 @@ export class EntryFormComponent {
   }
 
   prepareAndCreateTask () {
-    this.getExternals().pipe(
-      map((ext: TSelectExternals[]) => {
-        const taskData: ICreateTaskApi = {
-          title: this.form.value.title,
-          description: this.form.value.description,
-          status: prepareStatus4Api(this.form.value.status),
-          due_date: this.due_date,
-          externalServiceId: this.form.value.externalServiceId,
-          externalServiceName: getServiceNameById(ext, this.form.value.externalServiceId)
-        };
-        console.log(taskData)
-        return taskData
-      }),
-      switchMap(this.createTaskApi.bind(this))
-    ).subscribe({
-      next: (res: any) => {
-        console.log(res)
-      }
-    })
+    this.drawerService.show('loader')
+    const taskData: any | ICreateTaskApi = {
+      title: this.form.value.title,
+      description: this.form.value.description,
+      status: prepareStatus4Api(this.form.value.status),
+      due_date: this.due_date_result,
+      externalServiceId: this.form.value.externalServiceId,
+      externalServiceName: this.userExternalService.getServiceNameById(this.form.value.externalServiceId)
+    };
+    this.taskApi.createTask(taskData)
+    .pipe(
+      switchMap(() => this.taskService.getTasksByService(taskData.externalServiceId)),
+      catchError((err: any) => this.handleError(err)),
+      finalize(() => {
+        this.back()
+        this.drawerService.hide('loader')
+      })
+    )
+    .subscribe()
   }
 
-
-  createTaskApi(task: ICreateTaskApi) {
-
-    // const taskData = {
-    //   title: 'Example Task',
-    //   description: 'This is an example task description.',
-    //   status: 'In Progress',
-    //   due_date: '2022-12-31',
-    //   externalServiceId: 1,
-    //   externalServiceName: 'doro@github'
-    // };
-
-    return this.http.post(`${process.env['SERVER_URL']}/tasks`, task)
+  prepareAndEditTask() {
+    this.drawerService.show('loader')
+    const taskData: IEditTaskApi = {
+      title: this.form.value.title,
+      description: this.form.value.description,
+      status: prepareStatus4Api(this.form.value.status),
+      due_date: this.due_date_result,
+    };
+    this.taskApi.editTask(this.form.value.id, taskData)
+    .pipe(
+      switchMap(() => this.taskService.getTasksByService(this.form.value.externalServiceId)),
+      catchError((err: any) => this.handleError(err)),
+      finalize(() => {
+        this.back()
+        this.drawerService.hide('loader')
+      })
+    )
+    .subscribe()
   }
 
   private resetValidation() {
@@ -127,21 +181,12 @@ export class EntryFormComponent {
   }
 
   public onSelectDate (data: string) {
-    this.due_date = data
+    this.due_date_result = data
   }
 
-  private getExternals(): Observable<TSelectExternals[]> {
-    if (!this.externals$) {
-      this.externals$ = this.http.get<IGetUserExternalsApi[]>(`${process.env['SERVER_URL']}/userExternals`).pipe(
-        shareReplay(1),
-        map(res => {
-          return [
-            { id: -1, name: 'Проект' },
-            ...mapSelectOptions(res)
-          ]
-        })
-      );
-    }
-    return this.externals$;
+  private handleError (err: string | any): Observable<never> {
+    const errorMessage = err instanceof Error ? err.message : String(err)
+    console.error(errorMessage)
+    return EMPTY
   }
 }
