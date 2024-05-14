0dc88d1b90231a1ce277123dbcaba8cadb43e914
Antoshkin Artem <Artem.Antoshkin@infowatch.com>
2024-05-14 16:55:38 +0300

    * web: PLAN-0022 web-host https://red-kqjc.onrender.com/plan/get-ext-task/PLAN-0022

---
+++ web-host/README.md
@@ -0,0 +1,3 @@
+ng build --base-href /plan/
+
+localhost:3000/get-ext-task/PLAN-0012
---
+++ web-host/src/app/app.component.ts
@@ -0,0 +1,67 @@
+import { Component, Injector, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  template: `
+    <div class="mfeWrapper" #placeHolder></div>
+    <router-outlet></router-outlet>
+  `,
+  styles: [],
+})
+export class AppComponent implements OnInit{
+  data: any = null
+  @ViewChild('placeHolder', { read: ViewContainerRef }) viewContainer!: ViewContainerRef;
+
+  constructor(
+    private http: HttpClient
+  ) {
+    
+  }
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
+
+  async loadAuthComponent(jsonData: string): Promise<void> {
+
+    const m = await loadRemoteModule({
+      remoteName: 'plan',
+      remoteEntry: 'http://localhost:4205/remoteEntry.js',
+      exposedModule: './Component'
+    });
+
+    this.viewContainer.createComponent(
+      m.TaskReadonlyComponent,
+      {
+        injector: Injector.create({
+          providers: [
+            { provide: 'TASK_DATA', useValue: jsonData },
+            // { provide: PRODUCT_NAME, useValue: 'doro' },
+          ],
+        }),
+      }
+    );
+  }
+
+  fetchData(): void {
+    this.http.get('assets/data.json').subscribe({
+      next: (data: any) => {
+        this.data = data;
+        console.log('Data fetched:', this.data);
+        // You can now use this.data in your component
+        this.loadAuthComponent(this.data)
+      },
+      error: (error: any) => {
+        console.error('Error fetching data:', error);
+        // Handle the error appropriately
+      }
+    }
+    );
+  }
+}
